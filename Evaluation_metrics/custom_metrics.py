3fa833430f68ab25b0f13324ec867367a3eed09b
Gladiator07 <atharvaaingle@gmail.com>
2021-01-28 13:57:10 +0530

    micro and macro precision custom metric

--- Evaluation_metrics/custom_metrics.py
+++ Evaluation_metrics/custom_metrics.py
@@ -208,3 +208,84 @@ def log_loss(y_true, y_proba):
 
 # macro-averaged precision
 
+def macro_precision(y_true, y_pred):
+    """
+    Function to calculate macro averaged precision
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: macro precision score
+    """
+
+    # find the number of classes by taking length
+    # of unique values in true list
+
+    num_classes = len(np.unique(y_true))
+
+    # initialze precision to zero
+    precision = 0
+
+    # loop over all classes
+    for class_ in range(num_classes):
+
+        # all classes except current are considered negative
+        temp_true = [1 if p == class_ else 0 for p in y_true]
+        temp_pred = [1 if p == class_ else 0 for p in y_pred]
+
+        # calculate true positive for current class
+        tp = true_positive(temp_true, temp_pred)
+
+        # calculate false positive for current class
+        fp = false_positive(temp_true, temp_pred)
+
+        # calculate precision for current class
+        temp_precision = tp / (tp + fp)
+
+        # keep adding precision for all class
+        precision += temp_precision
+
+        # calculate and return average precision over all classes
+
+        precision /= num_classes
+
+        return precision
+
+
+# micro-precision
+
+def micro_precision(y_true, y_pred):
+    """
+   Function to calculate micro averaged precision
+   :param y_true: list of true values
+   :param y_pred: list of predicted values
+   :return: micro precision score
+   """
+    # find number of classes
+    num_classes = len(np.unique(y_true))
+
+    # initialize tp and fp to 0
+    tp = 0
+    fp = 0
+
+    # loop over all classes
+    for class_ in range(num_classes):
+        # all classes except current are considered negative
+        temp_true = [1 if p == class_ else 0 for p in y_true]
+        temp_pred = [1 if p == class_ else 0 for p in y_pred]
+
+        # calculate true positive for current class
+        # and update overall tp
+
+        tp += true_positive(temp_true, temp_pred)
+
+        # calculate false positive for current class
+        # and update overall tp
+
+        fp += false_positive(temp_true, temp_pred)
+
+        # calculate and return overall precision
+        precision = tp / (tp + fp)
+
+        return precision
+
+
+# weighted precision
