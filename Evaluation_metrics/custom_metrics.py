51380e0456ddb757557eb8846d9d96be4008498f
Gladiator07 <atharvaaingle@gmail.com>
2021-01-28 11:29:25 +0530

    log loss custom func

--- Evaluation_metrics/custom_metrics.py
+++ Evaluation_metrics/custom_metrics.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def accuracy(y_true, y_pred):
     """
     Function to calculate accuracy
@@ -12,10 +15,11 @@ def accuracy(y_true, y_pred):
     for yt, yp in zip(y_true, y_pred):
         if yt == yp:
             correct_counter += 1
-    
-    return correct_counter/ len(y_true)
+
+    return correct_counter / len(y_true)
 
 # function to calculate true positives
+
 
 def true_positive(y_true, y_pred):
     """
@@ -27,7 +31,7 @@ def true_positive(y_true, y_pred):
     # initialize counter
     tp = 0
     for yt, yp in zip(y_true, y_pred):
-        if yt==1 and yp==1:
+        if yt == 1 and yp == 1:
             tp += 1
     return tp
 
@@ -44,7 +48,7 @@ def true_negative(y_true, y_pred):
     # intialize the counter
     tn = 0
     for yt, yp in zip(y_true, y_pred):
-        if yt==0 and yp==0:
+        if yt == 0 and yp == 0:
             tn += 1
     return tn
 
@@ -61,7 +65,7 @@ def false_positive(y_true, y_pred):
     # intialize the counter
     fp = 0
     for yt, yp in zip(y_true, y_pred):
-        if yt==0 and yp==1:
+        if yt == 0 and yp == 1:
             fp += 1
     return fp
 
@@ -78,11 +82,12 @@ def false_negative(y_true, y_pred):
     # intialize the counter
     fn = 0
     for yt, yp in zip(y_true, y_pred):
-        if yt==1 and yp==0:
+        if yt == 1 and yp == 0:
             fn += 1
     return fn
 
 # accuracy using TP, FP, TN, FN
+
 
 def accuracy_v2(y_true, y_pred):
     """
@@ -117,6 +122,7 @@ def precision(y_true, y_pred):
 
 # function to calculate recall
 
+
 def recall(y_true, y_pred):
     """
     Function to calculate recall
@@ -128,6 +134,7 @@ def recall(y_true, y_pred):
     fn = false_negative(y_true, y_pred)
     recall = tp / (tp + fn)
     return recall
+
 
 def f1(y_true, y_pred):
     """
@@ -142,3 +149,62 @@ def f1(y_true, y_pred):
 
     score = 2 * p * r / (p + r)
     return score
+
+# true positive rate
+
+
+def tpr(y_true, y_pred):
+    """
+    Function to calculate tpr
+    :param y_true: list of true values
+    "param y_pred: list of predicted values
+    :return" tpr/recall
+    """
+
+    return recall(y_true, y_pred)
+
+
+def fpr(y_true, y_pred):
+    """
+    Function to calculate fpr
+    :param y_true: list of true values
+    "param y_pred: list of predicted values
+    :return" fpr
+    """
+    fp = false_positive(y_true, y_pred)
+    tn = true_negative(y_true, y_pred)
+
+    return fp / (tn + fp)
+
+# log-loss metric
+
+
+def log_loss(y_true, y_proba):
+    """
+    Function to calculate log loss
+    :param y_true: list of true values
+    :param y_proba: list of probabilities for 1
+    :return: overall log loss
+    """
+
+    # epsilon values (used to clip probabilities)
+    epsilon = 1e-15
+
+    loss = []
+
+    for yt, yp in zip(y_true, y_proba):
+        # adjust probability
+        # 0 gets converted to 1e-15
+        # 1 gets converted to 1-1e-15
+        yp = np.clip(yp, epsilon, 1-epsilon)
+        # calculate loss for one sample
+        temp_loss = -1.0 * (yt * np.log(yp) + (1 - yt) * np.log(1 - yp))
+
+        loss.append(temp_loss)
+
+    # return mean loss over all samples
+    return np.mean(loss)
+
+
+# macro-averaged precision
+
--- Evaluation_metrics/func_test.py
+++ Evaluation_metrics/func_test.py
@@ -1,12 +1,13 @@
 from custom_metrics import accuracy, accuracy_v2
-from custom_metrics import precision, recall,f1
+from custom_metrics import precision, recall, f1
 from sklearn.metrics import accuracy_score, f1_score
-
+from sklearn import metrics
+from custom_metrics import log_loss
 # checking both the accuracy function created with sci-kit learn implementation
 
 
-l1 = [0,1,1,1,0,0,0,1]
-l2 = [0,1,0,1,0,1,0,0]
+l1 = [0, 1, 1, 1, 0, 0, 0, 1]
+l2 = [0, 1, 0, 1, 0, 1, 0, 0]
 
 a1 = accuracy(l1, l2)
 a2 = accuracy_v2(l1, l2)
@@ -18,9 +19,24 @@ f1_s = f1_score(l1, l2)
 
 print(f"Accuracy of the first function: {a1}")
 print(f"Accuracy of the second(using tp,fp,fn,tn) function: {a2}")
-print(f"Accuracy of sklearn's implementation: {sk}") 
+print(f"Accuracy of sklearn's implementation: {sk}")
 
 print(f"Precision: {p}")
 print(f"Recall: {r}")
 print(f"f1 (custom metric): {f1_c}")
 print(f"f1 (sklearn metric): {f1_s}")
+
+
+# testing implementation of log loss
+
+y_true = [0, 0, 0, 0, 1, 0, 1,
+          0, 0, 1, 0, 1, 0, 0, 1]
+
+y_proba = [0.1, 0.3, 0.2, 0.6, 0.8, 0.05,
+           0.9, 0.5, 0.3, 0.66, 0.3, 0.2,
+           0.85, 0.15, 0.99]
+
+ll_custom = log_loss(y_true,y_proba)
+ll_sk = metrics.log_loss(y_true, y_proba)
+print(f"Log-loss (custom metric): {ll_custom}")
+print(f"Log-loss (sklearn metric): {ll_sk}")
