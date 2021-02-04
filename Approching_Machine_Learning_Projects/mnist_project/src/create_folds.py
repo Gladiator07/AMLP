82c28b7e274f7dd73033b8cc0213bd1532e13698
Gladiator07 <atharvaaingle@gmail.com>
2021-02-04 20:17:50 +0530

    updated main

--- Approching_Machine_Learning_Projects/mnist_project/src/create_folds.py
+++ Approching_Machine_Learning_Projects/mnist_project/src/create_folds.py
@@ -1,7 +1,7 @@
 from sklearn import model_selection
 import pandas as pd
 
-if __name__ == "main":
+if __name__ == "__main__":
     train_path = "/mnt/d/Approaching(Almost)_Any_MachineLearning_Problem/Approching_Machine_Learning_Projects/mnist_project/input/mnist_train.csv"
     df = pd.read_csv(train_path)
 
