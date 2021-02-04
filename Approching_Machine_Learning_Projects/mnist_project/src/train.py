0bdadec8ec959687a28e55297564d66d93d68a92
Gladiator07 <atharvaaingle@gmail.com>
2021-02-04 20:41:45 +0530

    argparse train pipeline

--- Approching_Machine_Learning_Projects/mnist_project/src/train.py
+++ Approching_Machine_Learning_Projects/mnist_project/src/train.py
@@ -2,14 +2,17 @@
 
 # Training script
 
+import argparse
+import os
+import config
 import joblib
 import pandas as pd
 from sklearn import metrics
 from sklearn import tree
 
+
 def run(fold):
-    train_folds_path = '../input/mnist_train_folds.csv'
-    df = pd.read_csv(train_folds_path)
+    df = pd.read_csv(config.TRAINING_FILE)
 
     # training data is where kfold is not equal to provided fold
     df_train = df[df.kfold != fold].reset_index(drop=True)
@@ -34,11 +37,37 @@ def run(fold):
     print(f"Fold={fold}, Accuracy={accuracy}")
 
     # save the model
-    joblib.dump(clf, f"../model/dt_{fold}.bin")
+    joblib.dump(clf,
+                os.path.join(config.MODEL_OUTPUT, f"../model/dt_{fold}.bin")
+                )
+
 
 if __name__ == "__main__":
-    run(fold=0)
-    run(fold=1)
-    run(fold=2)
-    run(fold=3)
-    run(fold=4)
+
+    # Bad way (may crash the program due to overloading memory)
+    # run(fold=0)
+    # run(fold=1)
+    # run(fold=2)
+    # run(fold=3)
+    # run(fold=4)
+
+    # Correct way to run folds
+
+    # initialize ArgumentParser class of argparse
+    parser = argparse.ArgumentParser()
+
+    # add different arguments you need and their type
+    # currently, we only need fold
+    parser.add_argument(
+        "--fold",
+        type=int
+    )
+    # read the argument from the command line
+    args = parser.parse_args()
+
+    # run the fold specified by command line arguments
+    run(fold=args.fold)
+
+
+# run the script by:
+# python train.py --fold 0
