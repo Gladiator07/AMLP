6dbc1894eb84c3b810faef3ab755b4fa28ed3d9f
Gladiator07 <atharvaaingle@gmail.com>
2021-02-07 20:30:46 +0530

    encoding train and test data

--- Categorical_Variables/prototype/encode.py
+++ Categorical_Variables/prototype/encode.py
@@ -1,17 +1,38 @@
-# encoding categorical features in dictionary (key-value pair)
-
-mapping = {
-    "Freezing": 0,
-    "Warm": 1,
-    "Cold": 2,
-    "Boiling Hot": 3,
-    "Hot": 4,
-    "Lava Hot": 5
-}
+# Encoding for rare variables which will occur in test data and not in training data
 
 import pandas as pd
+from sklearn import preprocessing
 
-df = pd.read_csv('../input/train.csv')
+train = pd.read_csv("../input/train.csv")
 
-df.loc[:, "ord_2"] = df.ord_2.map(mapping)
+test = pd.read_csv("../input/test.csv")
+
+# create a fake target column for test data 
+# since this column doesn't exist
+test.loc[:, "target"] = -1
+
+# concatenate both training and test data
+data = pd.concat([train, test]).reset_index(drop=True)
+
+# make a list of features we are interested in
+# id and target is something we should not encode
+
+features = [x for x in train.columns if x not in ["id", "target"]]
+
+# loop over the features list
+for feat in features:
+    # create a new instance of LabelEncoder for each feature
+    lbl_enc = preprocessing.LabelEncoder()
+
+    temp_col = data[feat].fillna("NONE").astype(str).values
+
+    # we can use fit_transform here as we do not 
+    # have any extra test data we need to 
+    # transform on separately
+
+    data.loc[:, feat] = lbl_enc.fit_transform(temp_col)
+
+# split the training and test data again
+train = data[data.target != -1].reset_index(drop=True)
+test = data[data.target == -1].reset_index(drop=True)
 
