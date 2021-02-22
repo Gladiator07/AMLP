da0792145f4f64edc7fe2d3b634765783122ca5c
Gladiator07 <atharvaaingle@gmail.com>
2021-02-22 20:22:13 +0530

    datetime feat create

---
+++ Feature_Engineering/datetime_feat.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# create a series of datetime with a frequency of 10 hours
+s = pd.date_range('2020-01-06', '2020-01-10', freq='10H').to_series()
+
+# create some features based on datetime
+
+features = {
+    "dayofweek": s.dt.dayofweek.values,
+    "dayofyear": s.dt.dayofyear.values,
+    "hour": s.dt.hour.values,
+    "is_leap_year": s.dt.is_leap_year.values,
+    "quarter": s.dt.quarter.values,
+    "weekofyear": s.dt.weekofyear.values
+}
+
+print(features)
