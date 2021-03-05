0c748666c4bf2e98627be91749a8a1a22a21fbf1
Gladiator07 <atharvaaingle@gmail.com>
2021-03-05 09:25:17 +0000

    setup

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "This script will set your kaggle api-key to Kaggle api and download data for image problems"
+
+echo "Installing dependencies"
+pip3 install --upgrade --force-reinstall --no-deps kaggle
+
+# Put your Kaggle api key path here
+echo "Fetching your Kaggle API Key"
+kaggle_api_key_path='/content/drive/MyDrive/Kaggle/kaggle.json'
+
+# This snippet will install kaggle api and connect your api-key to it
+mkdir -p ~/.kaggle
+echo "Setting up your Kaggle key to API..."
+cp $kaggle_api_key_path ~/.kaggle/
+cat ~/.kaggle/kaggle.json
+chmod 600 ~/.kaggle/kaggle.json
+echo "Kaggle API Key successfully linked !!!"
+
+# This snippet will download the data in specified folder
+
+# Specify the data path here
+# data_path="/content/AMLP/Image_problem/Classification/input"
+cd /content/AMLP/Image_problem/Classification/input
+kaggle competitions download -c siim-acr-pneumothorax-segmentation
+unzip siim-acr-pneumothorax-segmentation.zip
