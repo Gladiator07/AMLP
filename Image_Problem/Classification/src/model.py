b907493a0eb60a3d25569b23475a1a4b6c50e4d8
Gladiator07 <atharvaaingle@gmail.com>
2021-03-07 14:15:29 +0000

    model

---
+++ Image_Problem/Classification/src/model.py
@@ -0,0 +1,26 @@
+# model.py
+
+import torch.nn as nn
+import pretrainedmodels
+
+def get_model(pretrained):
+    if pretrained:
+        model = pretrainedmodels.__dict__["alexnet"](
+            pretrained='imagenet'
+        )
+    else:
+        model = pretrainedmodels.__dict__["alexnet"](
+            pretrained=None
+        )
+
+    model.last_linear = nn.Sequential(
+        nn.BatchNorm1d(4096),
+        nn.Dropout(p=0.25),
+        nn.Linear(in_features=4096, out_features=2048),
+        nn.ReLU(),
+        nn.BatchNorm1d(2048, eps=1e-05, momentum=0.1),
+        nn.Dropout(p=0.5),
+        nn.Linear(in_features=2048, out_features=1),
+    )
+
+    return model
